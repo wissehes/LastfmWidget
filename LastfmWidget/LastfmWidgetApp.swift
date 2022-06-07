ef2848486368faf8264128e1c5fa7a0bf27298a8
Wisse <wissehes@gmail.com>
2022-06-07 10:36:22 +0200

    Initial Commit

---
+++ LastfmWidget/LastfmWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastfmWidgetApp.swift
+//  LastfmWidget
+//
+//  Created by Wisse Hes on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LastfmWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
