f78308cfeb14f3587b7e62d0558dbb4676e000e3
Wisse <wissehes@gmail.com>
2022-06-09 20:10:06 +0200

    Add iOS App and move swift files to the shared folder

---
+++ LastFM Widgets/LastFM_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastFM_WidgetsApp.swift
+//  LastFM Widgets
+//
+//  Created by Wisse Hes on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LastFM_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
